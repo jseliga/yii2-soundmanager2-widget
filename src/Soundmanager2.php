23b23362f613e16fed870f6384c5aefbcb502186
jan <jan@git>
2016-01-27 15:57:42 +0100

    Added documentation comments

--- src/Soundmanager2.php
+++ src/Soundmanager2.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Soundmanager2 class for basic usage and creating widgets
+ *
+ * @package jseliga\soundmanager2
+ * @since 0.1.0
+ */
 
 namespace jseliga\soundmanager2;
 
@@ -11,32 +17,78 @@ use yii\web\AssetBundle;
 use yii\web\JsExpression;
 use yii\web\View;
 
+/**
+ * Base soundmanager2 class.
+ * @since 0.1.0
+ */
 class Soundmanager2 extends Widget {
+
+    /**
+     * Path to swf script.
+     * @var string
+     * @since 0.1.0
+     */
     const SWF_DIR = "/swf/";
 
-    const EVENT_ONREADY = "onready",
-        EVENT_ONLOAD = "onload";
+    /**
+     * Onready event.
+     * @var string
+     * @since 0.1.0
+     */
+    const EVENT_ONREADY = "onready";
 
+    /**
+     * Onload event.
+     * @var string
+     * @since 0.1.0
+     */
+    const EVENT_ONLOAD = "onload";
+
+    /**
+     * @var array Array of sounds.
+     * @since 0.1.0
+     */
     public $sounds = [];
 
+    /**
+     * @var array Global options for client script.
+     * @since 0.1.0
+     */
     public $clientOptions = [];
 
+    /**
+     * @var array Configuration for sounds.
+     * @since 0.1.0
+     */
     public $soundOptions = [];
 
     /**
-     * @var AssetBundle
+     * @var AssetBundle Registered AssetBundle.
+     * @since 0.1.0
      */
     protected $assetBundle;
 
+    /**
+     * @inheritdoc
+     */
     public function init() {
         $this->registerAssets();
     }
 
+    /**
+     * @inheritdoc
+     */
     public function run() {
         $this->registerClientScript();
     }
 
-    public function getSwfPath() {
+    /**
+     * Returns URL of swf script if assetBundle is registered.
+     * Otherwise returns null.
+     * @return null|string
+     * @since 0.1.0
+     */
+    public function getSwfUrl() {
         if ($this->assetBundle)  {
             return $this->assetBundle->baseUrl . self::SWF_DIR;
         }
@@ -44,18 +96,31 @@ class Soundmanager2 extends Widget {
         return null;
     }
 
+    /**
+     * Registers required assets.
+     * @since 0.1.0
+     */
     protected function registerAssets() {
         $this->assetBundle = Soundmanager2Asset::register($this->view);
     }
 
+    /**
+     * Registers client script.
+     * @since 0.1.0
+     */
     protected function registerClientScript() {
         $this->view->registerJs("soundManager.setup(" . Json::encode($this->getClientOptions()) . ");", View::POS_END);
     }
 
+    /**
+     * Merges client options with default values and returns it.
+     * @return array merged client options
+     * @since 0.1.0
+     */
     protected function getClientOptions() {
         $options = $this->clientOptions;
 
-        $options["url"] = $this->getSwfPath();
+        $options["url"] = $this->getSwfUrl();
 
         if (!ArrayHelper::keyExists(self::EVENT_ONREADY, $options)) {
             $options[self::EVENT_ONREADY] = $this->createSounds();
@@ -85,6 +150,12 @@ class Soundmanager2 extends Widget {
         ]);
     }
 
+    /**
+     * Merges sound options with default sound options and returns it.
+     * @param string $id id of sound
+     * @return array merged sound options
+     * @since 0.1.0
+     */
     protected function getSoundOptions($id = null) {
         $options = $this->soundOptions;
 
@@ -117,6 +188,11 @@ class Soundmanager2 extends Widget {
         ]);
     }
 
+    /**
+     * Initializes sounds and returns it.
+     * @return JsExpression initialized sounds
+     * @since 0.1.0
+     */
     protected function createSounds() {
         $sounds = "";
 
--- src/assets/Soundmanager2Asset.php
+++ src/assets/Soundmanager2Asset.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Base asset class for registering all required assets
+ *
+ * @package jseliga\soundmanager2\assets
+ * @since 0.1.0
+ */
 
 namespace jseliga\soundmanager2\assets;
 
@@ -6,11 +12,18 @@ use yii\web\AssetBundle;
 
 /**
  * Base assets for {@link jseliga\soundmanager2\Soundmanager2Widget}.
- * @package jseliga\soundmanager2\assets
+ * @since 0.1.0
  */
 class Soundmanager2Asset extends AssetBundle {
+
+    /**
+     * @var string Source path of soundmanager2 assets.
+     */
     public $sourcePath = "@bower/soundmanager2";
 
+    /**
+     * @var array Required javascript files.
+     */
     public $js = [
         "script/soundmanager2-jsmin.js"
     ];
